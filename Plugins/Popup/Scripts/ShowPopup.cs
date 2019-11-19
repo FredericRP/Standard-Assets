d3c753d78eedd5d3af4a66c894b61f357d6d2a60
FredericRP <frederic@rolland-porche.fr>
2019-11-19 21:57:56 +0100

    - NEW: Popup Management ! - FIX: transition animator call when exiting play mode caused a null ref exception

---
+++ Plugins/Popup/Scripts/ShowPopup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace FredericRP.Popups
+{
+	public class ShowPopup : MonoBehaviour
+	{
+		[SerializeField]
+		PopupDescriptor popup = null;
+
+		public void DisplayPopup()
+		{
+			PopupHandler.ShowPopup(popup);
+    }
+	}
+}
